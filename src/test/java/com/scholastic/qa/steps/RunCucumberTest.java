42731d457bb2f24e5f32c258fb9fdb0921069006
nathafor <nathafor@SCHMAC-NATHAFOR.local>
2015-06-04 14:37:30 -0400

    Fixed directory structure

--- src/test/java/com/scholastic/qa/steps/RunCucumberTest.java
+++ src/test/java/com/scholastic/qa/steps/RunCucumberTest.java
@@ -1,9 +1,12 @@
 package com.scholastic.qa.steps;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 
+@CucumberOptions(features = "src/test/resources/features/")
 public class RunCucumberTest {
 }
